ad2163d82e28dfd151ec654cb0759ba12f9fc5e3
Katsuki <Katsuki_Sato0130@icloud.com>
2018-10-18 07:23:11 +0900

    Updates

---
+++ precache-manifest.d9b018af1fd266a3e619e0c5cef9a1c2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6311c3affd0fe28667be",
+    "url": "/MyPortfolio/static/js/runtime~main.6311c3af.js"
+  },
+  {
+    "revision": "a38338bc85efe9992034",
+    "url": "/MyPortfolio/static/js/main.a38338bc.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/MyPortfolio/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "2733fe8282036bca405f05aaa84c1091",
+    "url": "/MyPortfolio/index.html"
+  }
+];
