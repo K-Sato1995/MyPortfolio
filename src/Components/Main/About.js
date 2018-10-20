51268c2e012c3a01563029f62c00f2a82f995ef7
Katsuki <Katsuki_Sato0130@icloud.com>
2018-10-20 13:06:02 +0900

    Abuot page design

--- src/Components/Main/About.js
+++ src/Components/Main/About.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../../Style/About.css';
 
 class About extends React.Component {
   render(){
     return (
-      <div>
-
+      <div className = 'aboutImage'>
+        <div className = 'layer'>
+          <div className = 'row about-container'>
+            <h1>About me</h1>
+            <div className = 'col-md-6 about-left'>
+              <div className = 'about-image-layer'></div>
+              <img src  = '../../../penguin.jpeg' className = 'about-me-image'/>
+            </div>
+            <div className = 'col-md-6 about-right'>
+              <div className = 'about-description'>
+                <p>People often find me outgoing, upbeat, selfmotivated and silly. I grew up in a very small and peaceful town in Japan and moved to a huge city for college.</p>
+                <p>In college, I majored in political economy and mainly spent my time there studying International political economy and Public economics. I also started coding when I was a senior and have been studying programming since then.</p>
+                <p>Though what I learnt in college was academically very interesting, I didn't necessarily think I am suitable for a politician or economist. I also was not interested in sales or marketing which a lot of my peers from college in the same department are doing. So I ended up getting a job as a webdeveloper since I really liked coding and I was really fascinated with the idea of createing something unique and original that didn't exist before.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+import Skills from './Skills';
+import Work from './Work';
+import Contact from './Contact';
 
 class Main extends React.Component {
   render(){
     return (
       <div className = 'container'>
-        <Route exact path= '/MyPortfolio' component={Home}/>
+        <Route exact path='/MyPortfolio' component={ Home } />
+        <Route exact path='/MyPortfolio/About' component={ About } />
+        <Route exact path='/MyPortfolio/Skills' component={ Skills } />
+        <Route exact path='/MyPortfolio/Work' component={ Work } />
+        <Route exact path='/MyPortfolio/Contact' component={ Contact } />
+        <p className = 'copy-right'>Copyright © K-Sato 2018</p>
+        <div className = 'link-list'>
+          <a href = 'https://k-blog0130.herokuapp.com/'>B</a>
+          <a href = 'https://github.com/K-Sato1995'><i className = 'fa fa-github'></i></a>
+          <a href = 'https://www.google.com/'><i className = 'fa fa-google'></i></a>
+          <a href = 'https://twitter.com/'><i className = 'fa fa-twitter'></i></a>
+        </div>
       </div>
     )
   }
