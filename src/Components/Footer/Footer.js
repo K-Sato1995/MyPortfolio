d25fd4cad7e6e9ba976068efee4ce4ed539b5600
Katsuki <Katsuki_Sato0130@icloud.com>
2018-11-03 10:44:47 +0900

    Changed footer;

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,12 +5,21 @@ class Footer extends React.Component {
   render(){
     return (
       <div className = 'footer'>
-        <p className = 'copy-right'>Copyright © K-Sato 2018</p>
-        <div className = 'link-list'>
-          <a href = 'https://k-blog0130.herokuapp.com/'>B</a>
-          <a href = 'https://github.com/K-Sato1995'><i className = 'fa fa-github'></i></a>
-          <a href = 'https://www.google.com/'><i className = 'fa fa-google'></i></a>
-          <a href = 'https://twitter.com/'><i className = 'fa fa-twitter'></i></a>
+        <div className = 'footer-container'>
+          <div className = 'footer-container-left'>
+            <h3>Get in touch</h3>
+            <p></p>
+          </div>
+          <div className = 'footer-container-right'>
+
+          </div>
+          <p className = 'copy-right'>Copyright © K-Sato 2018</p>
+          <div className = 'link-list'>
+            <a href = 'https://k-blog0130.herokuapp.com/'>B</a>
+            <a href = 'https://github.com/K-Sato1995'><i className = 'fa fa-github'></i></a>
+            <a href = 'https://www.google.com/'><i className = 'fa fa-google'></i></a>
+            <a href = 'https://twitter.com/'><i className = 'fa fa-twitter'></i></a>
+          </div>
         </div>
       </div>
     )
